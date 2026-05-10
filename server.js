5c694bd21da42131ab312580a10fc6ca5dcf401b
KevinHigueraCB <kevin.higuera@clickbalance.com>
2026-05-10 11:15:31 -0700

    Change port 3000 to 8080

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('<h1>Express with HTML</h1>'));
 
-app.listen(3000);
-console.log('Server is running on http://localhost:3000');
+app.listen(8080);
+console.log('Server is running on http://localhost:8080');
